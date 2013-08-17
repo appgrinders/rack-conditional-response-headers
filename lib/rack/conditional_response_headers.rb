3a1e5dc600a51da673f4a07ddd3da8bad1a5994c
Yarin Kessler <ykessler@gmail.com>
2013-08-17 18:40:03 -0400

    Initial commit

---
+++ lib/rack/conditional_response_headers.rb
@@ -0,0 +1,7 @@
+require "rack/conditional_response_headers/version"
+
+module Rack
+  module ConditionalResponseHeaders
+    # Your code goes here...
+  end
+end
---
+++ lib/rack/conditional_response_headers/version.rb
@@ -0,0 +1,5 @@
+module Rack
+  module ConditionalResponseHeaders
+    VERSION = "0.0.1"
+  end
+end
